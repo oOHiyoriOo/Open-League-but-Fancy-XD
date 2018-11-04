6a8a7a4b5aca1b1bce615cf962ef69929435a531
unknown <sidney.marvin.fricke@gmail.com>
2018-11-04 11:12:11 +0100

    Just a new step in direction Final

--- League.py
+++ League.py
@@ -1,63 +1,78 @@
 import subprocess
 import time
+import sys
 import os.path
-
 import colorama
 from colorama import init, Fore, Back, Style
-
 init(convert=True)
 
-x = "Welcome to "
-y = list(x)
-i = 0
-for each in y:
-    print(y[i], end="")
-    time.sleep(0.1)
-    i += 1
+try:
+    if os.path.isfile("path.txt"):
+        path_file = open("path.txt", "r")
+        for line in path_file:
+            print("Found Custom Path! using: "+line)
+    else:
+        print("No Custom Path found using Default")
 
-x = "League Of Legends"
-y = list(x)
-i = 0
-for each in y:
-    print(Fore.LIGHTBLUE_EX+ y[i], end="")
-    time.sleep(0.1)
-    i += 1
+    time.sleep(5)
+    print("\n"*10)
+    time.sleep(1000)
 
-
-print(Fore.WHITE+"\n")
-x = "Trying to open... "
-y = list(x)
-i = 0
-for each in y:
-    print(Fore.GREEN+ y[i], end="")
-    time.sleep(0.1)
-    i += 1
-
-path = "C:\\Riot Games\\League of Legends\\LeagueClient.exe"
-if os.path.isfile(path):
-    p = "exist"
-else:
-    p = "404"
-
-if p == "exist":
-    print(Fore.WHITE+"\n")
-    x = "Found!  "
+    x = "Welcome to "
     y = list(x)
     i = 0
     for each in y:
-        print(Fore.RED+ y[i], end="")
+        print(y[i], end="")
         time.sleep(0.1)
         i += 1
 
-else:
-    print(Fore.WHITE+"\n")
-    x = "Cant Find League!"
+    x = "League Of Legends"
     y = list(x)
     i = 0
     for each in y:
-        print(Fore.RED+ y[i], end="")
+        print(Fore.LIGHTBLUE_EX+ y[i], end="")
         time.sleep(0.1)
         i += 1
 
 
-# subprocess.call("cmd /c start explorer.exe \"C:\\Riot Games\\League of Legends\\ \"")
+    print(Fore.WHITE+"\n")
+    x = "Trying to open... "
+    y = list(x)
+    i = 0
+    for each in y:
+        print(Fore.GREEN+ y[i], end="")
+        time.sleep(0.1)
+        i += 1
+     
+    path = "C:\\Riot Games\\League of Legends\\LeagueClient.exe"
+    if os.path.isfile(path):
+        p = "exist"
+    else:
+        p = "404"
+
+    if p == "exist":
+        print(Fore.WHITE+"\n")
+        x = "Found!  "
+        y = list(x)
+        i = 0
+        for each in y:
+            print(Fore.RED+ y[i], end="")
+            time.sleep(0.1)
+            i += 1
+
+    else:
+        print(Fore.WHITE+"\n")
+        x = "Cant Find League!"
+        y = list(x)
+        i = 0
+        for each in y:
+            print(Fore.RED+ y[i], end="")
+            time.sleep(0.1)
+            i += 1
+
+
+    # subprocess.call("cmd /c start explorer.exe \"C:\\Riot Games\\League of Legends\\ \"")
+except KeyboardInterrupt:
+    print("\n"*10)
+    print(Fore.RED+"Exit")
+    sys.exit()
