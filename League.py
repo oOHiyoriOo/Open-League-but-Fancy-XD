4886c2515ade3037eaabad8f0f7b811ad26906b9
unknown <sidney.marvin.fricke@gmail.com>
2018-11-04 11:53:56 +0100

    Final ( added some timings and colors)

--- League.py
+++ League.py
@@ -10,14 +10,19 @@ try:
     if os.path.isfile("path.txt"):
         path_file = open("path.txt", "r")
         for line in path_file:
-            print("Found Custom Path! using: "+line)
+            print(Fore.YELLOW+"Found Custom Path! using: "+line)
     else:
-        print("No Custom Path found using Default")
+        print(Fore.YELLOW+"No Custom Path found using Default")
 
-    time.sleep(5)
-    print("\n"*10)
-    time.sleep(1000)
+    x = "\n \n \n \n \n \n \n \n \n \n \n \n \n"
+    y = list(x)
+    i = 0
+    for each in y:
+        print(y[i], end="")
+        time.sleep(0.1)
+        i += 1
 
+    print(Fore.WHITE)
     x = "Welcome to "
     y = list(x)
     i = 0
@@ -33,7 +38,8 @@ try:
         print(Fore.LIGHTBLUE_EX+ y[i], end="")
         time.sleep(0.1)
         i += 1
-
+    
+    time.sleep(1)
 
     print(Fore.WHITE+"\n")
     x = "Trying to open... "
@@ -43,22 +49,89 @@ try:
         print(Fore.GREEN+ y[i], end="")
         time.sleep(0.1)
         i += 1
-     
-    path = "C:\\Riot Games\\League of Legends\\LeagueClient.exe"
-    if os.path.isfile(path):
-        p = "exist"
-    else:
-        p = "404"
+    try:
+        line
+        path = line
+        folder = line.replace("LeagueClient.exe", "")
+    except NameError:
+        path = "C:\\Riot Games\\League of Legends\\LeagueClient.exe"
+        folder = path.replace("LeagueClient.exe", "")
+    
+    
+    conf = folder+"Config\\LeagueClientSettings.yaml"
+    
+    # print("\nconf: "+conf)
+    # print("path: "+path)
+    # print("folder: "+folder)
 
-    if p == "exist":
+    name = "user"
+    with open(conf, 'r') as conf_file:
+        line = conf_file.readline()
+        cnt = 0
+        for line in conf_file:
+            if "username" in line:
+                tmp, name = line.split(":")
+                break
+            cnt +=  1
+    time.sleep(2)
+
+    if os.path.isfile(path):
         print(Fore.WHITE+"\n")
-        x = "Found!  "
+        x = "Trying to load components!"
         y = list(x)
         i = 0
         for each in y:
             print(Fore.RED+ y[i], end="")
             time.sleep(0.1)
             i += 1
+        time.sleep(3)
+        print(Fore.WHITE+"\n")
+        x = "Initiating..."
+        y = list(x)
+        i = 0
+        for each in y:
+            print(Fore.YELLOW+ y[i], end="")
+            time.sleep(0.1)
+            i += 1
+        
+        print(Fore.WHITE+"\n")
+        x = ">>>Comepleted! Starting League with Profile "
+        y = list(x)
+        i = 0
+        for each in y:
+            print(Fore.GREEN+ y[i], end="")
+            time.sleep(0.1)
+            i += 1
+            
+        x = name
+        y = list(x)
+        i = 0
+        for each in y:
+            print(Fore.RED+ y[i], end="")
+            time.sleep(0.1)
+            i += 1
+
+        print(Fore.WHITE+"\n")
+        x = ". . . . . . . . . "
+        y = list(x)
+        i = 0
+        for each in y:
+            print(Fore.RED+ y[i], end="\r")
+            time.sleep(0.5)
+            i += 1
+        
+        subprocess.call("cmd /c \""+path+"\"")
+        
+        print(Fore.WHITE+"\n")
+        x = ">>> Started!"
+        y = list(x)
+        i = 0
+        for each in y:
+            print(Fore.RED+ y[i], end="")
+            time.sleep(0.5)
+            i += 1
+        time.sleep(3)
+        sys.exit()
 
     else:
         print(Fore.WHITE+"\n")
@@ -69,9 +142,6 @@ try:
             print(Fore.RED+ y[i], end="")
             time.sleep(0.1)
             i += 1
-
-
-    # subprocess.call("cmd /c start explorer.exe \"C:\\Riot Games\\League of Legends\\ \"")
 except KeyboardInterrupt:
     print("\n"*10)
     print(Fore.RED+"Exit")
